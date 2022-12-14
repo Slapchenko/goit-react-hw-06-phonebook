a8300a022501bdc38b0c49c703ab6307e21a8164
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-12-14 13:38:51 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,9 @@
-// import { useEffect, useState } from 'react';
 import { Section } from './Section';
 import { ContactForm } from './ContactForm';
 import { Filter } from './Filter';
 import { ContactList } from './ContactList';
-// import { nanoid } from 'nanoid';
 
 export function App() {
-  // const [contacts, setContacts] = useState([]);
-
-  // useEffect(() => {
-  //   const savedСontacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(savedСontacts);
-
-  //   if (parsedContacts) {
-  //     setContacts(parsedContacts);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <>
       <Section headTitle="Phonebook">
