31120ede2b08c12017c5a8ea75e824684b2115bb
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-12-14 07:53:42 +0000

    Update 2 files

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,9 @@
 // import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
+import { getVisibleContacts } from '../../helpers/visibleContacts';
 import { Contact } from './Contact';
 import { ContactListGroup, СontactСard } from './ContactList.styled';
-
-const getVisibleContacts = (contacts, filter) =>
-  contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
 
 export const ContactList = () => {
   const contacts = useSelector(getContacts);
@@ -18,12 +14,7 @@ export const ContactList = () => {
     <ContactListGroup>
       {visibleContacts.map(({ id, name, number }) => (
         <СontactСard key={id}>
-          <Contact
-            name={name}
-            number={number}
-            id={id}
-            // onDeleteContact={() => onDeleteContact(id)}
-          />
+          <Contact name={name} number={number} id={id} />
         </СontactСard>
       ))}
     </ContactListGroup>
